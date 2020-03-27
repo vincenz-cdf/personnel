d5735c91e296e27f74463d194d6c05d2f1278eec
klaus <alexandre.mesle@gmail.com>
2020-03-27 13:59:46 +0100

    Exemple d'utilisation de JDBC

--- Personnel/src/testsUnitaires/testLigue.java
+++ Personnel/src/testsUnitaires/testLigue.java
@@ -2,23 +2,26 @@ package testsUnitaires;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import personnel.*;
 
 class testLigue 
 {
+	GestionPersonnel gestionPersonnel = GestionPersonnel.getGestionPersonnel();
+	
 	@Test
-	void createLigue() 
+	void createLigue() throws SauvegardeImpossible
 	{
-		Ligue ligue = new Ligue("Fléchettes");
+		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
 		assertEquals("Fléchettes", ligue.getNom());
 	}
 
 	@Test
-	void addEmploye() 
+	void addEmploye() throws SauvegardeImpossible
 	{
-		Ligue ligue = new Ligue("Fléchettes");
+		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
 		Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty"); 
 		assertEquals(employe, ligue.getEmployes().first());
 	}
