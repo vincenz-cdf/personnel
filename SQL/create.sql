a2ec95185a8b5064575a30196b30594b92a78122
Vincent <85881442+vincenz-cdf@users.noreply.github.com>
2022-04-05 07:11:33 -0700

    mail varchar de 50 au lieu de 20

--- SQL/create.sql
+++ SQL/create.sql
@@ -14,7 +14,7 @@ CREATE TABLE employe(
      id_employe int NOT NULL AUTO_INCREMENT, 
      nom_employe VARCHAR(20), 
      prenom_employe VARCHAR(20),
-     mail_employe VARCHAR(20),
+     mail_employe VARCHAR(50),
      password_employe VARCHAR(20),
      date_arrivee DATE,
      date_depart DATE,
